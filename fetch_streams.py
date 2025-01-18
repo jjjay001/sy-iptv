782f66f7a1151eb1caaf4e57401bb5cff2be7be9
jjjay001 <35618991+jjjay001@users.noreply.github.com>
2025-01-18 22:36:35 +0800

    Update fetch_streams.py

--- fetch_streams.py
+++ fetch_streams.py
@@ -42,7 +42,7 @@ additional_sources = [
     ("凤凰资讯","http://aktv.top/AKTV/live/aktv/null-4/AKTV.m3u8"),
   #  ("寰宇新闻","http://aktv.top/AKTV/live/aktv/null-9/AKTV.m3u8"),
     ("香港卫视", "http://zhibo.hkstv.tv/livestream/mutfysrq/playlist.m3u8"),
-    ("龍華电影", "http://aktv.top/AKTV/live/aktv/null-23/AKTV.m3u8"),
+   # ("龍華电影", "http://aktv.top/AKTV/live/aktv/null-23/AKTV.m3u8"),
     ("翡翠台","http://aktv.top/AKTV/live/aktv/null/AKTV.m3u8"),
    # ("老高与小沫","https://www.goodiptv.club/douyu/236461"),
     ("group-title=\"影视轮播\",济公","https://lunbo.freetv.top/yy/1355265814"),
